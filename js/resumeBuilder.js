e2e27c015accc18ee652cf9b210ee98cdcd23935
Nick Hester <nickhstr@gmail.com>
2016-03-04 20:01:21 -0800

    Update as per suggestions

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,8 @@
-
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var data = "%data%";
+
 var bio = {
     "name": "Nick Hester",
     "role": "Web Developer",
@@ -11,38 +12,32 @@ var bio = {
         "github": "nickhstr",
         "location": "South San Francisco, CA"
     },
-    "biopic": "http://i1168.photobucket.com/albums/r498/nickhstr/FB_IMG_1447821803744_zps2zinmaw0.jpg",
-    "welcomeMsg": "Something, something, something, Dark Side.",
+    "welcomeMessage": "Something, something, something, Dark Side.",
     "skills": [
         "HTML", "CSS", "JavaScript", "Project Management", "Sales"
     ],
+    "biopic": "http://i1168.photobucket.com/albums/r498/nickhstr/FB_IMG_1447821803744_zps2zinmaw0.jpg",
     "display": function() {
-        var formattedName = HTMLheaderName.replace("%data%", bio.name);
-        var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-        var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-        var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-        var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
-        var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
-        var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-        var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
+        var formattedName = HTMLheaderName.replace(data, bio.name);
+        var formattedRole = HTMLheaderRole.replace(data, bio.role);
+        var formattedMobile = HTMLmobile.replace(data, bio.contacts.mobile);
+        var formattedEmail = HTMLemail.replace(data, bio.contacts.email);
+        var formattedBioPic = HTMLbioPic.replace(data, bio.biopic);
+        var formattedGithub = HTMLgithub.replace(data, bio.contacts.github);
+        var formattedLocation = HTMLlocation.replace(data, bio.contacts.location);
+        var formattedWelcomeMsg = HTMLwelcomeMsg.replace(data, bio.welcomeMessage);
 
         //Header
-        $("#header").prepend(formattedRole);
-        $("#header").prepend(formattedName);
-        $("#header").append(formattedBioPic);
-        $("#header").append(formattedWelcomeMsg);
+        $("#header").prepend(formattedRole).prepend(formattedName).append(formattedBioPic).append(formattedWelcomeMsg);
 
         //Contacts
-        $("#topContacts, #footerContacts").append(formattedMobile);
-        $("#topContacts, #footerContacts").append(formattedEmail);
-        $("#topContacts, #footerContacts").append(formattedGithub);
-        $("#topContacts, #footerContacts").append(formattedLocation);
-
+        $("#topContacts, #footerContacts").append(formattedMobile, formattedEmail, formattedGithub, formattedLocation);
+        
         //Skills
-        if (bio.skills.length > 0) {
+        if (bio.skills.length) {
             $("#header").append(HTMLskillsStart);
             bio.skills.forEach(function(skill) {
-                var formattedSkill = HTMLskills.replace("%data%", skill);
+                var formattedSkill = HTMLskills.replace(data, skill);
                 $("#skills").append(formattedSkill);
             });
         }
@@ -64,21 +59,21 @@ var work = {
         "description": "I need your help, Luke. She needs your help. I'm getting too old for this sort of thing. Ye-ha! In my experience, there is no such thing as luck. You mean it controls your actions?"
     }],
     "display": function() {
-        if (work.jobs.length > 0) {
+        if (work.jobs.length) {
             $("#workExperience").append(HTMLworkStart);
             work.jobs.forEach(function(job) {
-                var formattedEmployer = HTMLworkEmployer.replace("%data%", job.employer);
-                var formattedTitle = HTMLworkTitle.replace("%data%", job.title);
+                var formattedEmployer = HTMLworkEmployer.replace(data, job.employer);
+                var formattedTitle = HTMLworkTitle.replace(data, job.title);
                 var formattedEmployerTitle = formattedEmployer + formattedTitle;
                 $(".work-entry:last").append(formattedEmployerTitle);
 
-                var formattedDates = HTMLworkDates.replace("%data%", job.dates);
+                var formattedDates = HTMLworkDates.replace(data, job.dates);
                 $(".work-entry:last").append(formattedDates);
 
-                var formattedWorkLocation = HTMLworkLocation.replace("%data%", job.location);
+                var formattedWorkLocation = HTMLworkLocation.replace(data, job.location);
                 $(".work-entry:last").append(formattedWorkLocation);
 
-                var formattedWorkDescription = HTMLworkDescription.replace("%data%", job.description);
+                var formattedWorkDescription = HTMLworkDescription.replace(data, job.description);
                 $(".work-entry:last").append(formattedWorkDescription);
             });
         }
@@ -90,31 +85,35 @@ var projects = {
         "title": "Calculator",
         "dates": "2016",
         "description": "A simple JavaScript calculator. For those days when you and Math don't get along.",
-        "image": "images/calculator.jpg",
+        "images": [
+            "images/calculator.jpg"
+        ],
         "link": "http://codepen.io/nickhstr/full/BjPzLJ/"
     }, {
         "title": "Pomodoro Timer",
         "dates": "2016",
         "description": "A no-frills Pomodoro Timer. This tool reminds you to take a quick break from being a beast. Both the timer and break counts are customizable.",
-        "image": "images/timer.jpg",
+        "images": [
+            "images/timer.jpg"
+        ],
         "link": "http://codepen.io/nickhstr/full/qbMaJV/"
     }],
     "display": function() {
-        if (projects.projects.length > 0) {
+        if (projects.projects.length) {
             projects.projects.forEach(function(project) {
                 $("#projects").append(HTMLprojectStart);
 
-                var formattedTitle = HTMLprojectTitle.replace("#", project.link).replace("%data%", project.title);
-                $(".project-entry:last").append(formattedTitle);
+                var formattedTitle = HTMLprojectTitle.replace("#", project.link).replace(data, project.title);
+                var formattedDates = HTMLprojectDates.replace(data, project.dates);
+                var formattedDescription = HTMLprojectDescription.replace(data, project.description);
 
-                var formattedDates = HTMLprojectDates.replace("%data%", project.dates);
-                $(".project-entry:last").append(formattedDates);
+                $(".project-entry:last").append(formattedTitle).append(formattedDates).append(formattedDescription);
 
-                var formattedDescription = HTMLprojectDescription.replace("%data%", project.description);
-                $(".project-entry:last").append(formattedDescription);
+                project.images.forEach(function(image) {
+                    var formattedImage = HTMLprojectImage.replace(data, image);
 
-                var formattedImage = HTMLprojectImage.replace("%data%", project.image);
-                $(".project-entry:last").append(formattedImage);
+                    $(".project-entry:last").append(formattedImage);
+                });
             });
         }
     }
@@ -125,53 +124,55 @@ var education = {
         "name": "California State University, Chico",
         "location": "Chico, CA",
         "degree": "BS",
-        "majors": "Project Management, Applied Computer Graphics",
+        "majors": [
+            "Project Management",
+            "Applied Computer Graphics"
+        ],
         "dates": "2011 - 2015",
         "url": "http://www.csuchico.edu/"
     }],
     "onlineCourses": [{
         "title": "Front-End Web Developer Nanodegree",
         "school": "Udacity",
-        "dates": "March 2016 - Present",
+        "date": "March 2016 - Present",
         "url": "https://www.udacity.com/"
     }, {
         "title": "Full-Stack Development Certification",
         "school": "Free Code Camp",
-        "dates": "January 2016 - Present",
+        "date": "January 2016 - Present",
         "url": "http://www.freecodecamp.com/"
     }],
     "display": function() {
-        if (education.schools.length > 0) {
+        if (education.schools.length) {
             education.schools.forEach(function(school) {
                 $("#education").append(HTMLschoolStart);
 
-                var formattedName = HTMLschoolName.replace("%data%", school.name);
-                var formattedDegree = HTMLschoolDegree.replace("%data%", school.degree);
+                var formattedName = HTMLschoolName.replace(data, school.name);
+                var formattedDegree = HTMLschoolDegree.replace(data, school.degree);
                 var formattedNameDegree = formattedName + formattedDegree;
-                $(".education-entry:last").append(formattedNameDegree);
+                var formattedDates = HTMLschoolDates.replace(data, school.dates);
+                var formattedLocation = HTMLschoolLocation.replace(data, school.location);
 
-                var formattedDates = HTMLschoolDates.replace("%data%", school.dates);
-                $(".education-entry:last").append(formattedDates);
+                $(".education-entry:last").append(formattedNameDegree).append(formattedDates).append(formattedLocation);
 
-                var formattedLocation = HTMLschoolLocation.replace("%data%", school.location);
-                $(".education-entry:last").append(formattedLocation);
+                school.majors.forEach(function(major) {
+                    var formattedMajor = HTMLschoolMajor.replace(data, major);
 
-                var formattedMajors = HTMLschoolMajor.replace("%data%", school.majors);
-                $(".education-entry:last").append(formattedMajors);
+                    $(".education-entry:last").append(formattedMajor);
+                });
             });
         }
-        if (education.onlineCourses.length > 0) {
+        if (education.onlineCourses.length) {
             $("#education").append(HTMLonlineClasses);
             education.onlineCourses.forEach(function(course) {
                 $("#education").append(HTMLonlineStart);
 
-                var formattedTitle = HTMLonlineTitle.replace("#", course.url).replace("%data%", course.title);
-                var formattedSchool = HTMLonlineSchool.replace("%data%", course.school);
+                var formattedTitle = HTMLonlineTitle.replace("#", course.url).replace(data, course.title);
+                var formattedSchool = HTMLonlineSchool.replace(data, course.school);
                 var formattedTitleSchool = formattedTitle + formattedSchool;
-                $(".online-entry:last").append(formattedTitleSchool);
+                var formattedDate = HTMLonlineDates.replace(data, course.date);
 
-                var formattedDates = HTMLonlineDates.replace("%data%", course.dates);
-                $(".online-entry:last").append(formattedDates);
+                $(".online-entry:last").append(formattedTitleSchool).append(formattedDate);
             });
         }
     }
